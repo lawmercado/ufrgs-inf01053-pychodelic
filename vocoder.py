dd0326a099430a9931a90bc06560050b33732bdf
Luís Mercado <lawmercado@inf.ufrgs.br>
2020-10-31 00:45:43 -0300

    Code refactoring. Adding support for GitLFS.

--- vocoder.py
+++ vocoder.py
@@ -1,4 +1,6 @@
 import argparse
+from typing import List
+
 import librosa
 import matplotlib
 import matplotlib.pyplot as plt
@@ -7,17 +9,17 @@ import soundfile as sf
 
 from pychodelic.vocoder import vocode
 
-def savefig(filename, figlist, log=True):
-    n = len(figlist)
+def save_waves_plot(path: str, waves: List[np.ndarray]) -> None:
+    n = len(waves)
 
     plt.figure()
-    for i, f in enumerate(figlist):
+    for i, w in enumerate(waves):
         plt.subplot(n, 1, i+1)
-        if len(f.shape) == 1:
-            plt.plot(f)
-            plt.xlim([0, len(f)])
+        if len(w.shape) == 1:
+            plt.plot(w)
+            plt.xlim([0, len(w)])
             
-    plt.savefig(filename)
+    plt.savefig(path)
 
 parser = argparse.ArgumentParser(
     description="Modulates a carrier with a given modulator (i.e. creates a vocoder effect)."
@@ -29,6 +31,7 @@ parser.add_argument("output", type=str, help="Path where to save the resulting a
 parser.add_argument("--sampling_rate", type=int, help="Sampling rate to use when dealing with modulator and carrier audio files.", default=44100)
 parser.add_argument("--window_size", type=int, help="Window size to consider in the STFT.", default=2048)
 parser.add_argument("--window_overlap_size", type=int, help="Window overlap size to consider in the STFT.", default=1024)
+parser.add_argument("--plot", help="Wheter the waves should be plotted.", default=False, action="store_true")
 
 args = parser.parse_args()
 
@@ -43,7 +46,8 @@ carrier = carrier[0:len(modulator)]
 
 wave = vocode(modulator, carrier, args.sampling_rate, args.window_size, args.window_overlap_size)
 
-matplotlib.use("Agg")
-savefig(args.output + ".png", [modulator, carrier, wave])
-
 sf.write(args.output, wave, args.sampling_rate)
+
+if args.plot:
+    matplotlib.use("Agg")
+    save_waves_plot(args.output + ".png", [modulator, carrier, wave])
